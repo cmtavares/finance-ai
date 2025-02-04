e0c8aaa9e93f97f16dae235a81cbc8c5627b3a99
Carlos Tavares <cmmt08@gmail.com>
2025-02-04 15:11:42 -0300

    feat: add responsive design for small screens on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,7 +11,7 @@ const LoginPage = async () => {
     redirect("/");
   }
   return (
-    <div className="grid h-full grid-cols-2">
+    <div className="grid h-full grid-cols-1 md:grid-cols-2">
       {/* ESQUERDA */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
@@ -21,11 +21,11 @@ const LoginPage = async () => {
           alt="Finance AI"
           className="mb-8"
         />
-        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo!</h1>
         <p className="mb-8 text-muted-foreground">
-          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
-          monitorar suas movimentações, e oferecer insights personalizados,
-          facilitando o controle do seu orçamento.
+          A Finance AI é uma plataforma de <strong>gestão financeira</strong>{" "}
+          que utiliza IA para monitorar suas movimentações, e oferecer insights
+          personalizados, facilitando o controle do seu orçamento.
         </p>
         <SignInButton>
           <Button variant="outline" className="text-bold text-sm">
@@ -35,7 +35,7 @@ const LoginPage = async () => {
         </SignInButton>
       </div>
       {/* DIREITA */}
-      <div className="relative h-full w-full">
+      <div className="relative hidden h-full w-full md:block">
         <Image
           src="/login.png"
           alt="Faça login"
